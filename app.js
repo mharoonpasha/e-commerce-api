0ed23d1211e5c2603f191f71e0570daa75597741
haroon <mharoonpasha@hotmail.com>
2023-08-27 13:01:10 +0400

    second commit

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
+// security packages
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
 // database
 const connectDB = require("./db/connect");
 
@@ -23,6 +30,19 @@ const orderRouter = require("./routes/order-routes");
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// security
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan("tiny"));
 app.use(express.json());
