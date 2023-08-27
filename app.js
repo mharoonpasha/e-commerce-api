67f35a2ef32c1cc19e302afe8d53973abc29bc6a
haroon <mharoonpasha@hotmail.com>
2023-08-27 13:06:51 +0400

    third commit

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
@@ -52,15 +52,6 @@ app.use(express.static("./public"));
 app.use(fileUpload());
 
 // routes
-app.get("/", (req, res) => {
-  res.send("e-commerce api");
-});
-
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("e-commerce api");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
